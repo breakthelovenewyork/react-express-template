aba660e547ad4ed8041cb7b7b02dcc6e3dcd9d27
Christian J <5768801+christianjuth@users.noreply.github.com>
2024-07-29 13:58:24 -0400

    add cors

--- express/index.js
+++ express/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 const breeds = require('./data.js')
 const port = 4000
+
+app.use(cors())
 
 app.get('/breeds', (req, res) => {
   res.json(breeds)
